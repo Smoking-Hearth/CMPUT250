2b4c4330d705e774b65923d81895b99bd1488a89
Atonize <aamalia@ualberta.ca>
2025-01-22 23:19:44 -0700

    update: adds constricts to head, arm, and hose when aiming

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -7,38 +7,72 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] private float bulletInitialSpeed = 10.0f;
     [SerializeField] private float swingRadius;
     [SerializeField] private Vector2 swingPivotPosition;
-    [SerializeField] private Transform[] aimObjects;
+    [SerializeField] private SwingObject[] swingObjects;
     [SerializeField] private Transform flipObject;
     [SerializeField] private Transform nozzle;
-    private Vector2 startShootPosition;
+    private float aimAngle;
+
+    [System.Serializable]
+    private struct SwingObject
+    {
+        public Transform objectTransform;
+        public float minAngle;
+        public float maxAngle;
+        public float offsetAngle;
+    }
 
     private void FixedUpdate()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        startShootPosition = (Vector2)transform.position + swingRadius * (mousePosition - (Vector2)transform.position + swingPivotPosition).normalized;
 
         //For every object that needs to point towards the mouse
-        for (int i = 0; i < aimObjects.Length; i++) 
+        for (int i = 0; i < swingObjects.Length; i++) 
         {
             //Converting to an angle (need to conver radians to degrees)
-            float aimAngle = Mathf.Rad2Deg * Mathf.Atan2(mousePosition.y - aimObjects[i].position.y, mousePosition.x - aimObjects[i].position.x);
+            Vector2 direction = mousePosition - (Vector2)swingObjects[i].objectTransform.position;
+            aimAngle = Mathf.Rad2Deg * Mathf.Atan2(direction.y, direction.x);
 
             if (aimAngle <= 90 && aimAngle > -90)
             {
                 flipObject.localScale = Vector3.one;
-                aimObjects[i].rotation = Quaternion.Euler(0, 0, aimAngle);
+                float constrictAngle = aimAngle + 90;
+
+                if (constrictAngle > swingObjects[i].maxAngle)
+                {
+                    aimAngle = swingObjects[i].maxAngle - 90;
+                }
+                else if (constrictAngle < swingObjects[i].minAngle)
+                {
+                    aimAngle = swingObjects[i].minAngle - 90;
+                }
+                swingObjects[i].objectTransform.rotation = Quaternion.Euler(0, 0, aimAngle + swingObjects[i].offsetAngle);
             }
             else
             {
                 flipObject.localScale = new Vector3(-1, 1, 1);
-                aimObjects[i].rotation = Quaternion.Euler(0, 0, aimAngle + 180);
+                float constrictAngle = -aimAngle - 90;
+                if (constrictAngle < 0)
+                {
+                    constrictAngle = 360 + constrictAngle;
+                }
+
+                if (constrictAngle > swingObjects[i].maxAngle)
+                {
+                    aimAngle = -(swingObjects[i].maxAngle) - 90;
+                }
+                else if (constrictAngle < swingObjects[i].minAngle)
+                {
+                    aimAngle = -(swingObjects[i].minAngle) - 90;
+                }
+
+                swingObjects[i].objectTransform.rotation = Quaternion.Euler(0, 0, aimAngle + 180 - swingObjects[i].offsetAngle);
             }
         }
     }
 
     public void Shoot(Vector2 targetPosition)
     {
-        Vector2 shootDirection = targetPosition - startShootPosition;
+        Vector2 shootDirection = Quaternion.Euler(0, 0, aimAngle) * Vector2.right;
         GameObject firedBullet = Instantiate(bullet, nozzle.position, Quaternion.identity);
 
         var rigidBody = firedBullet.GetComponent<Rigidbody2D>();
