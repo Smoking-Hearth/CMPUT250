7cb3cd050e22abea663e4f7902120fe3ce214f2a
GenericConfluent <m13power@gmail.com>
2025-01-25 16:42:27 -0700

    refactor: pull scene management from `MainMenu`

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,40 +1,116 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
-    enum SceneName 
+    [Flags]
+    public enum SceneMask
     {
         None = 0,
         Settings = 1 << 0,
         Forest = 1 << 1,
+        Suburbs = 1 << 2,
+        City = 1 << 3,
     }
 
+    SceneMask loaded = SceneMask.None;
+    public SceneMask Loaded 
+    {
+        get { return loaded; }
+    }
+
+    public enum TransitionType
+    {
+        // It seems like our concept in the opening requires this.
+        // Since the camera is in the sky with just the top of the 
+        // watchtower in view, and then it tracks down to show the 
+        // player standing at the bottom after play is hit.
+        Continuous,
+        Fade,
+        Jump
+    }
+    
+    static SceneLoader instance;
+    public static SceneLoader Instance
+    {
+        get 
+        { 
+            if (instance != null) return instance; 
+            instance = new GameObject("SceneLoader").AddComponent<SceneLoader>();
+            return instance;
+        }
+    }
+
+    public void Transition(SceneMask scenes, TransitionType type)
+    {
+        if (scenes == SceneMask.None) return;
+        if (scenes == SceneMask.Forest) 
+        {
+            SceneManager.LoadSceneAsync("Forest");
+        }
+    }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            // Scene Loader was manually added to the scene.
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (this != Instance)
+        {
+            // Somehow we ended up with a duplicate.
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            // Instance was created upon request in the getter.
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += SceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= SceneLoaded;
+    }
+
+    void SceneLoaded(Scene loaded, LoadSceneMode mode)
+    {
+        SceneManager.MergeScenes(loaded, SceneManager.GetActiveScene());
+    }
+}
+
+public class MainMenu : MonoBehaviour
+{
     [SerializeField] Button play, settings;
 
-    SceneName transitionTo = SceneName.None;
-    AsyncOperation loadStatus = null;
-
-    void Start()
+    void OnEnable()
     {
         play.onClick.AddListener(OnPlayClick);
         settings.onClick.AddListener(OnSettingsClick);
-        loadStatus = SceneManager.LoadSceneAsync("Forest"); 
     }
 
-    // Update is called once per frame
+    void OnDisable()
+    {
+        play.onClick.RemoveListener(OnPlayClick);
+        settings.onClick.RemoveListener(OnSettingsClick);
+    }
+
     void OnPlayClick()
     {
-        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Forest"));
+        SceneLoader.Instance.Transition(SceneLoader.SceneMask.Forest, SceneLoader.TransitionType.Jump);
     }
 
     void OnSettingsClick()
     {
-    }
-
-    void SceneLoaded()
-    {
-
+        SceneLoader.Instance.Transition(SceneLoader.SceneMask.Settings, SceneLoader.TransitionType.Jump);
     }
 }
