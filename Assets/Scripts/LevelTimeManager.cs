e8cec8cc072c587260aa7b09b4cccb9021e4c513
Atonize <aamalia@ualberta.ca>
2025-02-04 15:05:56 -0700

    feat: class to track remaining time
    Still needs to be added to GameManager

---
+++ Assets/Scripts/LevelTimeManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimeManager
+{
+    private float timeLimitSeconds;
+    private float remainingSeconds;
+    private Slider progressBar;
+
+    public delegate void OnTimeout();
+    public event OnTimeout onTimeout;
+
+    public LevelTimeManager(float limit, Slider bar)
+    {
+        timeLimitSeconds = limit;
+        remainingSeconds = timeLimitSeconds;
+        progressBar = bar;
+        progressBar.maxValue = timeLimitSeconds;
+        UpdateBar();
+    }
+
+    private void UpdateBar()
+    {
+        if (remainingSeconds < 0)
+        {
+            remainingSeconds = 0;
+        }
+        progressBar.value = remainingSeconds;
+    }
+
+    public void DepleteTime(float seconds)
+    {
+        remainingSeconds -= seconds;
+        UpdateBar();
+
+        if (remainingSeconds <= 0 && onTimeout != null)
+        {
+            onTimeout();
+        }
+    }
+}
