e995f2ed880798e97db37c4353bd57af694dd6b0
Atonize <aamalia@ualberta.ca>
2025-02-08 12:59:02 -0700

    feat: auto continue option for dialogue

--- Assets/Scripts/DialogueHolder.cs
+++ Assets/Scripts/DialogueHolder.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class DialogueHolder : MonoBehaviour
 {
-    [SerializeField] private string title;
+    [SerializeField] protected string title;
     [TextArea(0, 5)]
-    [SerializeField] private List<string> lines;
+    [SerializeField] protected List<string> lines;
+    [SerializeField] protected bool autoContinue;
 
     public void PlayDialogue()
     {
-        GameManager.dialogSystem.Play(new GameDialog(lines, title));
+        GameManager.dialogSystem.Play(new GameDialog(lines, title), autoContinue);
     }
 }
