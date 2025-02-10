ed8cdaaf2225969bc91297ef2f2bae8b6f275cd9
Matthew Power <m13power@gmail.com>
2025-02-09 17:26:54 -0700

    feat!: demo over screen

---
+++ Assets/Scripts/UI/TechDemo.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TechDemo : MonoBehaviour 
+{
+    public void BackToMenu()
+    {
+        // I've really gotta fix this mess.
+        GameManager.cameraAnimator.Play("MainMenu");
+        Unquenchable.SceneManager.UnloadHook(SceneManager.GetActiveScene());
+        StartCoroutine(Unquenchable.SceneManager.SetSceneActive(SceneIndex.MainMenu));
+    }
+}
