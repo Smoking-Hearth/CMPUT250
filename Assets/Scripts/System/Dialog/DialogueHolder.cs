c5df1027b6a027001f1a54aedd704f8ae4318e78
Matthew Power <m13power@gmail.com>
2025-02-23 09:39:06 -0700

    chore: copy dialog into forest scene

--- Assets/Scripts/System/Dialog/DialogueHolder.cs
+++ Assets/Scripts/System/Dialog/DialogueHolder.cs
@@ -1,15 +1,22 @@
-using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public struct DialogSegment
+{
+    public string title;
+
+    [TextArea(0, 5)]
+    public string[] lines;
+
+    public bool autoContinue;
+}
 
 public class DialogueHolder : MonoBehaviour
 {
-    [SerializeField] protected string title;
-    [TextArea(0, 5)]
-    [SerializeField] protected List<string> lines;
-    [SerializeField] protected bool autoContinue;
+    [SerializeField] protected DialogSegment[] lines;
 
     public void PlayDialogue()
     {
-        gameObject.MyLevelManager().DialogSystem.Play(new GameDialog(lines, title), autoContinue);
+        // gameObject.MyLevelManager().DialogSystem.Play(new GameDialog(lines, title), autoContinue);
     }
 }
