0c357af515e328117f4396eb7dfa47e34af81406
Matthew Power <m13power@gmail.com>
2025-02-23 10:28:35 -0700

    feat(checkpoint): save arbitrary state

--- Assets/Scripts/System/CheckpointSystem.cs
+++ Assets/Scripts/System/CheckpointSystem.cs
@@ -2,6 +2,12 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct SavedVariableAccess
+{
+    public Func<object> Get;
+    public Action<object> Set;
+}
+
 [Serializable]
 public class CheckpointSystem: MonoBehaviour
 {
@@ -20,6 +26,39 @@ public class CheckpointSystem: MonoBehaviour
 
     [SerializeField] private float triggerDist;
 
+    private List<SavedVariableAccess> savedState;
+    private List<object> savedData;
+
+    void RegisterState(SavedVariableAccess savedVariableAccess)
+    {
+        if (savedState == null)
+        {
+            savedState = new() { savedVariableAccess };
+            savedData = new() { null };
+        }
+        else
+        {
+            savedState.Add(savedVariableAccess);
+            savedData.Add(null);
+        }
+    }
+
+    public void SaveState()
+    {
+        for (int i = 0; i < savedState.Count; ++i)
+        {
+            savedData[i] = savedState[i].Get();
+        }
+    }
+
+    public void LoadState()
+    {
+        for (int i = 0; i < savedState.Count; ++i)
+        {
+            savedState[i].Set(savedData[i]);
+        }
+    }
+
     void FixedUpdate()
     {
         Vector2 playerPos = gameObject.MyLevelManager().Player.Position;
@@ -28,8 +67,7 @@ public class CheckpointSystem: MonoBehaviour
             if (Vector2.Distance(checkpoints[i].position, playerPos) <= triggerDist)
             {
                 current = i;
-                // Debug.Log("New checkpoint appointed: "+ checkpoints[current].position);
-                // Debug.Log("Current: " + current);
+                SaveState();
             }
         }
     }
