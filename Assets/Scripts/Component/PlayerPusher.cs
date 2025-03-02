6b22c032b8448d74ad9d2f9c98bcc41494b3e1ea
Atonize <aamalia@ualberta.ca>
2025-03-01 17:18:52 -0700

    feat: broken fire hydrant

---
+++ Assets/Scripts/Component/PlayerPusher.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerPusher : MonoBehaviour
+{
+    [SerializeField] private Bounds bounds;
+    [SerializeField] private Vector2 force;
+
+    private void Start()
+    {
+        bounds.center += transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Player player = gameObject.MyLevelManager().Player;
+        if (bounds.Contains(player.Position))
+        {
+            player.Movement.PushPlayer(force);
+        }
+    }
+}
