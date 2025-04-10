3d1766074a981267d24009c9fdc2ad6002d29dc7
Matthew Power <m13power@gmail.com>
2025-04-09 20:57:11 -0600

    fix: broken menu and re-center

--- Assets/Scripts/UI/DifficultyScreen.cs
+++ Assets/Scripts/UI/DifficultyScreen.cs
@@ -14,28 +14,22 @@ public class DifficultyScreenScript: MonoBehaviour
     public void easyDifficulty(){
         
         PlayerShoot.DamageMultiplier = 1.5f;
-        Debug.Log("Easy: Damage Multiplier set to 1.5f");
+        DevLog.Info("Easy: Damage Multiplier set to 1.5f");
         difficultyText.text = "Easy";
 
     }
     public void normalDifficulty(){
         
         PlayerShoot.DamageMultiplier = 1f;
-        Debug.Log("Normal: Damage Multiplier set to 1f");
+        DevLog.Info("Normal: Damage Multiplier set to 1f");
         difficultyText.text = "Normal";
 
     }
     public void hardDifficulty(){
         
         PlayerShoot.DamageMultiplier = 0.65f;
-        Debug.Log("Hard: Damage Multiplier set to 0.65f");
+        DevLog.Info("Hard: Damage Multiplier set to 0.65f");
         difficultyText.text = "Hard";
 
-    }
-
-    void OnBackClick()
-    {
-        GameManager.Instance.StartCoroutine(GameManager.SceneSystem.GoBack());
-        GameManager.Instance.StartCoroutine(GameManager.SceneSystem.Unload((int)SceneIndex.Credits));
     }
 }
