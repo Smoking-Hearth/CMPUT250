c738a005d00c47b404d6d011324e1708565bfcb6
Matthew Power <m13power@gmail.com>
2025-04-09 18:07:43 -0600

    fix: bar span independent of bar level

--- Assets/Scripts/UI/AnimatedBar.cs
+++ Assets/Scripts/UI/AnimatedBar.cs
@@ -7,8 +7,7 @@ public class AnimatedBar : MonoBehaviour
 {
     enum State {
         Stable, 
-        IntensityIncreasing,
-        IntensityDecreasing,
+        Approach,
     }
 
     private State state = State.Stable;
@@ -33,17 +32,15 @@ public class AnimatedBar : MonoBehaviour
     }
 
 
-    private float barPosition = 1f;
-
-    private float lossIntensity = 0.2f;
-
-    [SerializeField] private float current = 1f;
+    private float intensity = 0.03f;
+    private float lagEdge = 1f;
+    [SerializeField] private float leadEdge = 1f;
     public float Current 
     {
-        get { return current;}
+        get { return leadEdge;}
         set 
         {
-            current = Mathf.Clamp(value, minValue, maxValue);
+            leadEdge = Mathf.Clamp(value, minValue, maxValue);
         }
     }
 
@@ -52,62 +49,54 @@ public class AnimatedBar : MonoBehaviour
     [SerializeField] private float intensityDecreasePoint = 0.3f;
 
     // This is where the display shows the health to be at.
-    private MotionHandle barPosAnim = MotionHandle.None;
+    private MotionHandle lagEdgeAnim = MotionHandle.None;
     private MotionHandle intensityAnim = MotionHandle.None;
 
     private void Awake()
     {
-        current = Mathf.Clamp(current, minValue, maxValue);
-        barPosition = current;
+        leadEdge = Mathf.Clamp(leadEdge, minValue, maxValue);
+        lagEdge = leadEdge;
         state = State.Stable;
         UpdateUniforms();
     }
 
     private void UpdateUniforms()
     {
-        float normalizedPosition = barPosition / (maxValue - minValue);
-        height.material.SetFloat("_BarPosition", normalizedPosition);
-        height.material.SetFloat("_Intensity", lossIntensity);
-    }
-
-    private void SetIntensity(float val)
-    {
-        lossIntensity = val;
+        float lagEdgeNorm = lagEdge / (maxValue - minValue);
+        float leadEdgeNorm = leadEdge / (maxValue - minValue);
+        float lo = Mathf.Min(lagEdgeNorm, leadEdgeNorm);
+        float hi = Mathf.Max(lagEdgeNorm, leadEdgeNorm);
+        height.material.SetFloat("_Lo", lo);
+        height.material.SetFloat("_Hi", hi);
+        height.material.SetFloat("_Intensity", intensity);
     }
 
     private void StableUpdate()
     {
-        if (Mathf.Abs(barPosition - current) < 0.02) return;
-        
-        state = State.IntensityIncreasing;
-        
-        barPosAnim = LMotion.Create(barPosition, current, duration)
-            .WithEase(Ease.OutQuad)
-            .Bind(val => barPosition = val);
+        if (Mathf.Abs(lagEdge - leadEdge) < 1e-3) return;
 
-        intensityAnim = LMotion.Create(lossIntensity, 1f, intensityDecreasePoint)
-            .Bind(SetIntensity);
+        state = State.Approach;
+        
+        intensityAnim = LMotion.Create(0.3f, 0.03f, duration * 1.1f)
+            .WithEase(Ease.Linear)
+            .Bind(val => intensity = val);
+
+        lagEdgeAnim = LMotion.Create(lagEdge, leadEdge, duration)
+            .WithEase(Ease.Linear)
+            .Bind(val => lagEdge = val);
     }
 
     public void Update()
     {
-        if (state != State.Stable) UpdateUniforms();
-
         switch (state)
         {
             case State.Stable:
                 StableUpdate();
                 break;
 
-            case State.IntensityIncreasing:
-                if (intensityAnim.IsPlaying()) return;
-                state = State.IntensityDecreasing;
-                intensityAnim = LMotion.Create(lossIntensity, 0.2f, duration - intensityDecreasePoint)
-                    .Bind(SetIntensity);
-                break;
-
-            case State.IntensityDecreasing:
-                if (intensityAnim.IsPlaying() || barPosAnim.IsPlaying()) return;
+            case State.Approach:
+                UpdateUniforms();
+                if (lagEdgeAnim.IsPlaying() || intensityAnim.IsPlaying()) return;
                 state = State.Stable;
                 break;
         }
